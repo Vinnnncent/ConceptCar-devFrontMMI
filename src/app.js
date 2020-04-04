d401547886948e5dfbda3ec767c1fa066706a805
Vinnnncent <vincent.joulain2@gmail.com>
2020-04-04 13:27:03 +0200

    [another update]

--- src/app.js
+++ src/app.js
@@ -4,61 +4,61 @@ var leftParallax = document.getElementsByClassName("leftParallax");
 var rightParallax = document.getElementsByClassName("rightParallax");
 
 var overflowRight = document.getElementsByClassName("overflowRight");
-var overflowdown = document.getElementsByClassName("overflowDown");
+// var overflowdown = document.getElementsByClassName("overflowDown");
 
 new simpleParallax(downParallax, {
-  scale: 2
+  scale: 2,
 });
 new simpleParallax(leftParallax, {
-  orientation: "left"
+  orientation: "left",
 });
 new simpleParallax(rightParallax, {
   orientation: "right",
-  scale: 3
+  scale: 3,
 });
 new simpleParallax(overflowRight, {
   overflow: true,
   orientation: "right",
-  scale: 4
+  scale: 4,
 });
-new simpleParallax(overflowDown, {
-  overflow: true,
-  orientation: "down",
-  scale: 4
-});
+// new simpleParallax(overflowDown, {
+//   overflow: true,
+//   orientation: "down",
+//   scale: 4
+// });
 
 var img1 = new Waypoint({
   element: document.getElementById("img1"),
-  handler: function(direction) {
-    this.element.classList.add("imgLastForward");
+  handler: function (direction) {
+    this.element.classList.add("imgAppear");
     console.log(direction);
   },
-  offset: "75%"
+  offset: "75%",
 });
 
 var img2 = new Waypoint({
   element: document.getElementById("img2"),
-  handler: function(direction) {
-    this.element.classList.add("imgLastBackward");
+  handler: function (direction) {
+    this.element.classList.add("imgAppear");
     console.log(direction);
   },
-  offset: "75%"
+  offset: "75%",
 });
 
 var img3 = new Waypoint({
   element: document.getElementById("img3"),
-  handler: function(direction) {
-    this.element.classList.add("imgLastForward");
+  handler: function (direction) {
+    this.element.classList.add("imgAppear");
     console.log(direction);
   },
-  offset: "75%"
+  offset: "75%",
 });
 
 var img4 = new Waypoint({
   element: document.getElementById("img4"),
-  handler: function(direction) {
-    this.element.classList.add("imgLastBackward");
+  handler: function (direction) {
+    this.element.classList.add("imgAppear");
     console.log(direction);
   },
-  offset: "75%"
+  offset: "75%",
 });
